81a64398c928c78535cafcb2a5c61798c42064c7
Nick Davis <hire@nicholasdavis.co>
2017-09-24 11:29:39 -0400

    Starting to build out the flex-layout structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
 import { MaterialModule } from '@angular/material';
 import {FlexLayoutModule} from "@angular/flex-layout";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { PropertyComponent } from './property/property.component';
+
+const appRoutes: Routes = [
+    {
+        path: 'property',
+        component: PropertyComponent,
+    },
+    //{ path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
     declarations: [
@@ -19,7 +28,10 @@ import { PropertyComponent } from './property/property.component';
         AppRoutingModule,
         MaterialModule,
         FlexLayoutModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        RouterModule.forRoot(
+            appRoutes
+        )
     ],
     providers: [],
     bootstrap: [AppComponent]
