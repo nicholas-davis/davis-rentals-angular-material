d37e49a66ff809d7c3709f162766538e79c7dbab
Nick Davis <hire@nicholasdavis.co>
2017-10-29 19:02:20 -0400

    Setting up property map placeholder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { AppComponent } from './app.component';
 import { MaterialModule } from '@angular/material';
 import {FlexLayoutModule} from "@angular/flex-layout";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AgmCoreModule } from '@agm/core';
+
 import { PropertyComponent } from './property/property.component';
 import { ListingComponent } from './property/listing/listing.component';
 
@@ -22,7 +24,10 @@ import { ListingComponent } from './property/listing/listing.component';
         AppRoutingModule,
         MaterialModule,
         FlexLayoutModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        AgmCoreModule.forRoot({
+            apiKey: 'AIzaSyBSnJiWUwA6Dt7qI8BzY9y_cTql_yLmEpE'
+        })
     ],
     providers: [],
     bootstrap: [AppComponent]
--- src/app/property/property.component.ts
+++ src/app/property/property.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-property',
-  templateUrl: './property.component.html',
-  styleUrls: ['./property.component.scss']
+    selector: 'app-property',
+    templateUrl: './property.component.html',
+    styleUrls: ['./property.component.scss'],
+    host: { 'class': 'property' }
 })
 export class PropertyComponent implements OnInit {
+    lat: number = 39.9281788;
+    lng: number = -75.16583880000002;
+    zoom: number = 16;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
