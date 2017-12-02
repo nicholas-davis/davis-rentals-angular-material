39a726c40dcbf835892691938111214f525d66e4
Nick Davis <hire@nicholasdavis.co>
2017-12-02 16:30:44 -0500

    properties details service and module

---
+++ src/app/property/listing/details/details.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//Services
+import { DetailsService } from './details.service';
+
+@NgModule({
+    imports: [
+        CommonModule
+    ],
+    declarations: [],
+    providers: [
+        DetailsService
+    ],
+})
+export class DetailsModule { }
---
+++ src/app/property/listing/details/details.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class DetailsService {
+
+    constructor(
+        private http: HttpClient
+    ) { }
+
+    getPropertyDetails() {
+        return this.http.get('api/mock-property-details.json');
+    }
+
+}
